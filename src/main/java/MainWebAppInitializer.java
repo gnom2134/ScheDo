376a891246d8b409548a6bbe02a663872c68d461
Daniil Masalskiy <gnom21345@gmail.com>
2018-12-05 13:53:32 +0300

    Initial commit

---
+++ src/main/java/MainWebAppInitializer.java
@@ -0,0 +1,25 @@
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.jsp.jstl.core.Config;
+
+public class MainWebAppInitializer implements WebApplicationInitializer {
+    public void onStartup(final ServletContext sc) throws ServletException{
+        AnnotationConfigWebApplicationContext root =
+                new AnnotationConfigWebApplicationContext();
+
+        root.scan("com.baeldung");
+        sc.addListener(new ContextLoaderListener(root));
+
+        ServletRegistration.Dynamic appServlet =
+                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
+    }
+}
---
+++ src/main/java/com/baeldung/ClientWebConfig.java
@@ -0,0 +1,32 @@
+package com.baeldung;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.baeldung.web.controller" })
+public class ClientWebConfig implements WebMvcConfigurer{
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("index");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+
+        return bean;
+    }
+}
